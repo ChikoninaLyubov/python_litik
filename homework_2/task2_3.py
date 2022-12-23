f8638f7a69715f495eb2789625b11e76fb349514
lyubov <lyubov.chikonina@mail.ru>
2022-12-23 07:52:50 +0300

    Программа 3 задачи homework2

--- homework_2/task2_3.py
+++ homework_2/task2_3.py
@@ -1,3 +1,17 @@
 # 3. Реализуйте алгоритм перемешивания списка.
 # НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE,
 # максимум использование библиотеки Random для получения случайного int
+num = int(input('Введите цифру : '))
+num_list = list(range(num))
+my_list = []
+print(num_list)
+from random import randrange
+
+for i in range(num):
+    n_1, n_2 = randrange(num), randrange(num)
+    num_list[n_1], num_list[n_2] = num_list[n_2], num_list[n_1]
+
+print(num_list)
+
+
+
