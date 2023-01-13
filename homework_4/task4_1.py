e6dc18dab2a20be1ecd5789130b49153af6cd288
lyubov <lyubov.chikonina@mail.ru>
2023-01-13 13:15:03 +0300

    исправления программа 1 задачи homowork4

--- homework_4/task4_1.py
+++ homework_4/task4_1.py
@@ -1,40 +1,26 @@
-#1. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
-# Пример:если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-# from random import randrange, choice
+# 1. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:если k = 2, то многочлены могут быть =>
+#  2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+from random import choice, sample
 
-import random
 
-# создаем словарь: ключ значение ( от 0 до 100)
-def create_dictionary():
-    degree = int(input('Enter the k - degree:'))
-    my_dict ={}
-    for n in range(degree, -1, -1):
-        my_dict[n] = random.randint(0,101)
-    return my_dict
+def polynomial(num: int):
+    if num < 1:
+        return 0
 
-def polynomial(my_dict:dict) -> str:
-    new_equation = []
-    for key, value in my_dict.items():
-        if value != 0:
-            new_equation.append(f'{value}*x**{key}')
-    new_equation = ' + '.join(new_equation) + ' = 0'
-    # new_equation = new_equation.replace('+-', '-'), тк от 0 до 100 будет только плюс
-    new_equation = new_equation.replace(' 1*x', ' x')
-    new_equation = new_equation.replace('*x**0', '')
-    new_equation = new_equation.replace('x**1', 'x')
-    return new_equation
+    poly = ""
+    num_list = range(0, 11)
 
-# new_equation=polynomial(my_dict)
-# print(new_equation)
+    with open("poly_2.txt", "a", encoding="utf-8") as my_f:
+        for i in range(num, 1, -1):
+            value = choice(num_list)
+            if value:
+                poly += f"{value}*x^{i} {choice('+-')} "
 
-def main():
-   
-    my_dict = create_dictionary()
-    print(my_dict)
+        numbers = sample(range(1, 11), k=2)
+        my_f.write(f"{poly}{choice(numbers)}*x {choice('+-')} {choice(numbers)} = 0\n")
 
-    new_equation1=polynomial(my_dict)
-    print(new_equation1)
- 
 
-if __name__ == "__main__":
-    main()    
+for _ in range(3):
+    polynomial(int(input()))
